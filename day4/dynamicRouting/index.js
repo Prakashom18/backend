8865e2a6c7f64463465f32caa021c4ef6b373f70
Om Prakash Shah <shahomprakash097@gmail.com>
2025-03-04 23:23:36 +0545

    day4

---
+++ day4/dynamicRouting/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+// app.set("view engine", ejs);
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.get("/profile/:username", (req, res) => {
+  res.send(`welcome,${req.params.username}`);
+});
+app.get("/author/:username/:age", (req, res) => {
+  res.send(`welcome ${req.params.username} and age ${req.params.age}`);
+});
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
