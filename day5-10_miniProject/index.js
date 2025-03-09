106b13ca66718488cd527cda21e57dcf7438bb26
Om Prakash Shah <shahomprakash097@gmail.com>
2025-03-09 23:19:40 +0545

    mini project

---
+++ day5-10_miniProject/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  fs.readdir(`./files`, function (err, files) {
+    console.log(files);
+    res.render("index", { files: files });
+  });
+});
+
+app.get("/files/:filename",(req,res)=>{
+    fs.readFile(`./files/${req.params.filename}`,"utf-8",(err,filedata)=>{
+      console.log(filedata)
+    res.render("show",{filename:req.params.filename,filedata:filedata});
+    })
+})
+app.get("/edit/:filename",(req,res)=>{
+  res.render('edit',{filename:req.params.filename});
+})
+
+app.post("/edit",(req,res)=>{
+  console.log(req.body);
+  fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,(err)=>{
+    console.log(err);
+    res.redirect("/");
+  });
+})
+
+app.post("/create",(req,res)=>{
+  console.log(req.body);
+  fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,(err)=>{
+    res.redirect('/');
+  });
+})
+
+app.listen(3000, () => {
+  console.log("running in port 3000");
+});
